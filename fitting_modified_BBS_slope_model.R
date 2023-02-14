4f4789d2df1581637567724f2b901d211729138a
Smith <Adam.Smith@ec.gc.ca>
2023-02-14 11:21:40 -0500

    fitting modified spatial slope model to BBS data for selected species

---
+++ fitting_modified_BBS_slope_model.R
@@ -0,0 +1,124 @@
+### creating a slope-only model for comparison with
+### a similar eBird model
+
+#setwd("C:/Users/SmithAC/Documents/GitHub/bbsBayes2_modified_slope")
+
+library(bbsBayes2)
+library(tidyverse)
+
+
+model = "slope"
+stratification = "latlong"
+model_variant = "spatial"
+firstYear = 2007
+lastYear = 2018
+
+
+# # copy bbsBayes2 model file to directory
+# # only done once, not necessary to repeat
+# copy_model_file(model = model,
+#                 model_variant = model_variant,
+#                 dir = "models")
+
+### The above exported Stan file was modified manually
+### to remove or quote-out all uses of the yeareffects parameters
+### the resulting file was stored as the slope_only version referenced
+### in the next line.
+alternate_model_file <- "models/slope_only_spatial_bbs_CV.stan"
+
+
+sps_to_run <- c("Wood Thrush",
+                "Ovenbird",
+                "Red-eyed Vireo",
+                "Barn Swallow",
+                "Baltimore Oriole",
+                "Black-capped Chickadee",
+                "Carolina Chickadee",
+                "American Woodcock",
+                "Hairy Woodpecker",
+                "Indigo Bunting",
+                "Lazuli Bunting",
+                "Ruffed Grouse",
+                "Upland Sandpiper")
+
+for(species in sps_to_run){
+#species = "Red-eyed Vireo"
+
+
+s <- stratify(by = stratification,
+              species = species)
+
+## alternate minimum route-years for AMWO
+## however, even with this low number, the strata map is
+## a bit ridiculous
+if(species == "American Woodcock"){
+p <- prepare_data(s,
+                  min_n_routes = 1,
+                  min_max_route_years = 2,
+                  min_year = firstYear,
+                  max_year = lastYear)
+}else{
+  p <- prepare_data(s,
+                    min_n_routes = 1,
+                    min_max_route_years = 3,
+                    min_year = firstYear,
+                    max_year = lastYear)
+}
+
+ps <- prepare_spatial(p,
+                      strata_map = load_map(stratification),
+                      label_size = 1)
+
+print(ps$spatial_data$map)
+
+pm <- prepare_model(ps,
+                    model = model,
+                    model_variant = model_variant,
+                    model_file = alternate_model_file)
+
+fit <- run_model(pm,
+                 refresh = 200,
+                 max_treedepth = 13, #
+                 adapt_delta = 0.8,#initial try with defaults
+                 output_dir = "output",
+                 output_basename = paste(species,stratification,model,model_variant,sep = "_"))
+#bbsBayes2 saves full cmdstanr fit object in 
+# rds file stored as
+# saveRDS(fit,paste0(output_dir,output_basename,".rds))
+
+
+# this is a wrapper around stanfit$summary()
+summ <- get_summary(fit) #full parameter summary including convergence diagnostics
+saveRDS(summ,file = paste0("output/",paste(species,stratification,model,model_variant,sep = "_"),"param_summ.rds"))
+}
+
+
+
+# Accessing the saved cmdstanr fit object ---------------------------------
+
+
+for(species in sps_to_run){
+  #bbsBayes2 saves full cmdstanr fit object in 
+  # rds file
+  
+  bbsBayes2_fit <- readRDS(paste0("output/",
+                                  paste(species,stratification,model,model_variant,sep = "_"),
+                                  ".rds"))
+  
+  # this is the CmdStanMCMC fit object
+  stanfit <- bbsBayes2_fit$model_fit
+  
+  # the rest of the bbsBayes2_fit object is the data
+  # spatial information, etc.
+  
+  
+  ## cmdstanr$summary() object
+  summ <- readRDS(file = paste0("output/",paste(species,stratification,model,model_variant,sep = "_"),"param_summ.rds"))
+  
+}
+
+
+
+
+
+
